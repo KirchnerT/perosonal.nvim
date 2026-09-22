d78d3c84e26c0463be644b74a7311d08bf6c8c77
Tyler Kirchner <49655028+KirchnerT@users.noreply.github.com>
2026-09-21 19:09:32 -0500

    Update to theme

--- init.lua
+++ init.lua
@@ -824,23 +824,27 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
+    'catppuccin/nvim',
+    name = 'catppuccin',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
-      ---@diagnostic disable-next-line: missing-fields
-      require('tokyonight').setup {
-        styles = {
-          comments = { italic = false }, -- Disable italics in comments
-          sidebars = 'transparent',
-          floats = 'transparent',
+      require('catppuccin').setup {
+        flavour = 'mocha', -- latte, frappe, macchiato, mocha
+        transparent_background = true,
+        integrations = {
+          neotree = true,
+          telescope = true,
+          which_key = true,
+          gitsigns = true,
+          mason = true,
+          native_lsp = { enabled = true },
         },
-        transparent = true,
       }
 
       -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      -- Other flavours you could load instead: 'catppuccin-latte', 'catppuccin-frappe',
+      -- 'catppuccin-macchiato', or just 'catppuccin' to use the flavour set above.
+      vim.cmd.colorscheme 'catppuccin'
     end,
   },
 
